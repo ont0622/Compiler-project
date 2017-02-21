2f678972cf2018438db762450d7a66d3bb6c5be3
Oh Daniel <Oh Daniel>
2017-02-21 17:54:45 -0500

    Updated to take file names and read contents.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 /*
-    Daniel Oh
+    Daniel Oh, John Wayne Creech
     CSC 4340
 
     This is a main file for WAE program
@@ -12,44 +12,37 @@ import java.util.*;
 public class Main {
   static public void main(String argv[]) throws ArrayIndexOutOfBoundsException {
     /* Start the parser */
-
-    if (argv.length == 0){
-      try{
-        System.out.print("WAE> ");
-        String next;
-        StringBuffer userInput = new StringBuffer();
-        Scanner in = new Scanner(System.in);
-        PrintWriter out = new PrintWriter("testFile.txt");
-        do{
-          next = in.next();
-          userInput.append(next);
-        }
-        while(!next.equals(";"));
-        out.append(userInput);
-        out.close();
-        parser p = new parser(new Lexer(new FileReader("testFile.txt")));
-        Object result = p.parse().value;
-      }
-      catch (Error e){
-        e.printStackTrace();
-      }
-      catch (Exception e){
-        e.printStackTrace();
-      }
-    }
-    else{
       try {
-        parser p = new parser(new Lexer(new FileReader(argv[0])));
-        Object result = p.parse().value;
-      }
-      catch (Error e){
+          System.out.print("SQL> ");
+          String next="";
+          String userInput = "";
+          Scanner in = new Scanner(System.in);
+          while(!userInput.contains(";")){
+            userInput += in.next();
+            System.out.println("Current String: "+ userInput);
+          }
+          System.out.println("Final input: "+userInput);
+          if (userInput.contains("@")){
+            String fileName;
+            fileName = userInput.replace("@", "");
+            fileName = fileName.replace(";", "");
+            fileName += ".txt";
+            System.out.println("File name: " + fileName);
+            parser p = new parser(new Lexer(new FileReader(fileName)));
+            Object result = p.parse().value;
+          }
+          else {
+            Reader reader = new StringReader(userInput);
+            parser p = new parser(reader);
+            Object result = p.parse().value;
+          }
+      } catch (Error e) {
         e.printStackTrace();
-      }
-      catch (Exception e) {
+      } catch (FileNotFoundException e){
+        e.printStackTrace();
+      } catch (Exception e) {
         e.printStackTrace();
       }
 
     }
-
-  }
 }
