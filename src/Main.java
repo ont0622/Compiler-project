a57327d3e4b4aed7a8028b241c1761aa4658998b
Oh Daniel <Oh Daniel>
2017-02-23 21:14:54 -0500

    comments added

--- src/Main.java
+++ src/Main.java
@@ -16,19 +16,29 @@ public class Main {
           System.out.print("SQL> ");
           String next="";
           String userInput = "";
+          /**
+           * ArrayList is used to store user inputs from command argument.
+           * It will later concatonate into a single string and use that string as an argument
+           * for parser
+           */
           ArrayList<String> inputList = new ArrayList<String>();
           Scanner in = new Scanner(System.in);
+          //to prevent the nullpointerexception, inputList[0] will be an empty string
           inputList.add(" ");
           while(!inputList.get(inputList.size()-1).contains(";")){
             inputList.add(in.next());
           }
+
+          //Turning user inputs into single string seperated by empty space
           for (int i = 1; i < inputList.size(); i++){
               userInput += inputList.get(i) + " ";
           }
+          //if userInput has sequence "exit", the program will terminate
           if (userInput.contains("exit")){
               in.close();
               System.exit(1);
           }
+          //if userInput is an file name, it will parse using input file name
           if (userInput.contains("@")){
             String fileName;
             fileName = userInput.replace("@", "");
@@ -39,6 +49,7 @@ public class Main {
             parser p = new parser(new Lexer(new FileReader(fileName)));
             Object result = p.parse().value;
           }
+          //otherwise it will parse with input string
           else {
             Reader reader = new StringReader(userInput);
             parser p = new parser(reader);
