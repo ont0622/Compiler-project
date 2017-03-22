b2734ab24a4790586eff57f525066e36d051abf1
Oh Daniel <Oh Daniel>
2017-03-22 11:27:21 -0400

    clean up

--- src/Column.java
+++
@@ -1,6 +0,0 @@
-public class Column{
-    String prefix; //contains prefix, if any
-    String name; //contains column name
-    SQLNode sn; //contains SQLNode where column resolves
-    int ci; //contains index of relations where column resolves
-}
